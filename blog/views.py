4ed38d2fd2ce2a176931ab78bd91d741aea0404b
jstaliano <jstaliano@gmail.com>
2020-04-21 11:35:28 -0300

    git add git commit -m "Detail and Update View" git push -f -u origin master

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView,DetailView,UpdateView
+from django.views.generic.edit import CreateView
+from . models import Post
 # Create your views here.
+
+class BlogListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+    #context_object_name = 'custom'
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'blog/post_new.html'
+    #context_object_name = 'custom'
+    fields = ('autor','slug','titulo','conteudo')
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'blog/post_edit.html'
+    #context_object_name = 'custom'
+    fields = ('titulo','conteudo')
+
